dc16f69677679341153328e22cdd89f9c3952f90
Abduraxim <toxirmalk@gmail.com>
2025-01-11 15:14:38 +0500

    33

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ dotenv.config();
   imports: [
     AuthModule,
     MongooseModule.forRoot(
-      process.env.MONGO_URI || 'mongodb://localhost:27017/test',
+      process.env.MONGO_URI || 'mongodb://localhost:27018/test',
     ),
   ],
 })
